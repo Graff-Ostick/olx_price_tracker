37ae9bbfa18df3ef64e9f3a93558623e760d92bb
Ruslan_Shebak <ruslan.shebak@gmail.com>
2024-11-30 23:39:39 +0200

    - create structure

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php
+
+
+require_once 'src/Database.php';
+require_once 'src/SubscriptionService.php';
+
+$db = new Database('sqlite:../database/price_tracker.sqlite');
+$subscriptionService = new SubscriptionService($db);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $url = $_POST['ad_url'];
+    $email = $_POST['email'];
+
+    $response = $subscriptionService->subscribe($url, $email);
+    echo json_encode($response);
+}
