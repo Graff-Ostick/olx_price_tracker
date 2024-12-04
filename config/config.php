7569f23e1e59318a0452796a3358ed32c3a41f7b
Ruslan_Shebak <ruslan.shebak@gmail.com>
2024-12-04 21:56:43 +0200

    - make structure

---
+++ config/config.php
@@ -0,0 +1,15 @@
+<?php
+return [
+    'db' => [
+        'host' => '',
+        'name' => '',
+        'user' => '',
+        'password' => '',
+    ],
+    'smtp' => [
+        'host' => 'smtp.gmail.com',
+        'port' => 587,
+        'username' => '@gmail.com',
+        'password' => '',
+    ],
+];
--- cron/check_prices.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../src/Database.php';
-require_once '../src/PriceTrackerService.php';
-
-$db = new Database('sqlite:../database/price_tracker.sqlite');
-$tracker = new PriceTrackerService($db);
-
-$tracker->checkPrices();
